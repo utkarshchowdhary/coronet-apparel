1e48cc46668c01d9810096ac254d8618f8b90073
Utkarsh Chowdhary <utkarshchowdhary4140@gmail.com>
2022-07-06 16:34:19 +0530

    removed redux-logger in favour of redux-devtools-extension

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
@@ -18,14 +17,9 @@ const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const sagaMiddleware = createSagaMiddleware()
 
-const middlewares = [
-  process.env.NODE_ENV === 'development' && logger,
-  sagaMiddleware
-].filter(Boolean)
-
 const store = createStore(
   persistedReducer,
-  composeWithDevTools(applyMiddleware(...middlewares))
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
 )
 
 sagaMiddleware.run(rootSaga)
